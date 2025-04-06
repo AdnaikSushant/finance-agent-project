03dca1efc965813e8b03918aaa4f465f3c71a829
Sushant Adnaik <91546867+AdnaikSushant@users.noreply.github.com>
2025-04-07 00:15:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from phi.agent import Agent
 from phi.model.groq import Groq
 import os
 
-os.environ["GROQ_API_KEY"] = "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"
+os.environ["GROQ_API_KEY"] = ""
 
 agent=Agent(
     model = Groq(id='llama3-8b-8192',api_key=os.environ["GROQ_API_KEY"]))
